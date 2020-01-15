eb14fdc170d4a01e6b1035d59df19b8ce1adafe6
georgi.nikolov <georgi.nikolov@mobisystems.com>
2020-01-15 16:36:53 +0200

    Fixes

--- SampleApp/app/src/main/java/com/alfastack/sampleapp/threads/DataFetcher.kt
+++ SampleApp/app/src/main/java/com/alfastack/sampleapp/threads/DataFetcher.kt
@@ -3,7 +3,7 @@ package com.alfastack.sampleapp.threads
 import android.os.Looper
 import com.alfastack.sampleapp.models.Employee
 import com.alfastack.sampleapp.models.Flow
-import org.json.JSONArray
+import org.json.JSONObject
 import java.io.BufferedReader
 import java.net.URL
 import javax.net.ssl.HttpsURLConnection
@@ -27,8 +27,8 @@ internal class DataFetcher(private val flow: Flow, private val mLock: Object) : 
             val httpsURLConnection = url.openConnection() as HttpsURLConnection
             val allText =
                 httpsURLConnection.inputStream.bufferedReader().use(BufferedReader::readText)
-            val array = JSONArray(allText)
-            flow.response = array
+            val json = JSONObject(allText)
+            flow.response = json.getJSONArray("data")
             mLock.notify()
             mLock.wait()
             handler.post { onFinishedCallback(flow.employees) }
